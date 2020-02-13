65d281587f4ac0ff2198931bacb75cd2c54cf98d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-13 14:40:20 +0100

    Improve checks

--- test.py
+++ test.py
@@ -1,42 +1,53 @@
+import csv
+from pathlib import Path
+
 import pint
+from pint.util import UnitsContainer
 
 import pytest
 
 
-D_ENERGY = {'[length]': 2.0, '[mass]': 1.0, '[time]': -2.0}
-
-# Expected units: each tuple is (unit string, dimensionality, True if defined
-# by this package)
-PARAMS = [
-    ('toe', D_ENERGY, False),
-    ('tce', D_ENERGY, True),
-    ('GW a', D_ENERGY, False),
-]
+defaults = pint.get_application_registry()
 
 
-# Fixtures and utilities for tests
-@pytest.fixture(scope='session')
-def empty_registry():
-    yield pint.UnitRegistry()
+# Read units to check from file
+PARAMS = []
+with open(Path(__file__).with_name('checks.csv')) as f:
+    for row in csv.reader(f, skipinitialspace=True, quoting=csv.QUOTE_MINIMAL):
+        try:
+            unit_str, dims, new_def = row
+        except ValueError:
+            continue  # Comment or malformed row; skip
+        else:
+            if unit_str.startswith('#'):
+                continue  # Comment; skip
+
+        # Convert second column to a dimensionality object
+        dims = {dims: 1} if dims.startswith('[') else eval(dims)
+        dims = defaults.get_dimensionality(UnitsContainer(dims))
+
+        # Convert third column to boolean
+        new_def = len(new_def) > 0
+
+        # Store
+        PARAMS.append((unit_str, dims, new_def))
 
 
 @pytest.fixture(scope='session')
 def registry():
+    """UnitRegistry including definitions from definitions.txt."""
     reg = pint.UnitRegistry()
-    reg.load_definitions('definitions.txt')
+    reg.load_definitions(str(Path(__file__).with_name('definitions.txt')))
     yield reg
 
 
-def idfn(val):
-    return val if isinstance(val, str) else ''
-
-
-@pytest.mark.parametrize('unit_name, dims, new_def', PARAMS, ids=idfn)
-def test_units(empty_registry, registry, unit_name, dims, new_def):
+@pytest.mark.parametrize('unit_str, dim, new_def', PARAMS,
+                         ids=lambda v: v if isinstance(v, str) else '')
+def test_units(registry, unit_str, dim, new_def):
     if new_def:
         # Units defined in dimensions.txt are not recognized by base pint
         with pytest.raises(pint.UndefinedUnitError):
-            empty_registry('1 ' + unit_name)
+            defaults('1 ' + unit_str)
 
     # Units can be parsed and have the correct dimensionality
-    assert registry('1 ' + unit_name).dimensionality == dims
+    assert registry('1 ' + unit_str).dimensionality == dim
