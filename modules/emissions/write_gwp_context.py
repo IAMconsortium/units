79ed60c3722fff5a4b124842946d687b6c9e5f7f
Daniel Huppmann <dh@dergelbesalon.at>
2020-03-12 17:18:44 +0100

    update context-script to insert species-headers (requested by @gidden)

--- modules/emissions/write_gwp_context.py
+++ modules/emissions/write_gwp_context.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-context = 'SARGWP100'
+# list of contexts
+contexts = ['SARGWP100', 'AR4GWP100', 'AR5GWP100']
 
 # add explicit conversion mappings for derived units (with time & mass)
 formatters = [
@@ -17,25 +18,32 @@ data = (
         .dropna(subset=['Species'])
 )
 
-# check that context exists as column in the table
-if context not in data.columns:
-    raise ValueError(f'no column `{context}` in data file')
 
-# downselect to required columns, drop na
-data = data[['Species', 'Symbol', context]].dropna()
+for context in contexts:
+    # check that context exists as column in the table
+    if context not in data.columns:
+        raise ValueError(f'no column `{context}` in data file')
 
-# write to file
-gwp = open(f'gwp_{context}.txt', 'w')
-gwp.writelines(f'@context gwp_{context} \n \n')
+    # downselect to required columns, drop na
+    _data = data[['Species', 'Symbol', context]].dropna()
 
-for i, (species, symbol, value) in data.iterrows():
-    for f in formatters:
-        f_species = f.format(species)
-        f_co2 = f.format('carbon_dioxide')
-        gwp.write(
-            f'    {f_species} -> {f_co2}: value * {value} * CO2 / {symbol} \n')
-        gwp.write(
-            f'    {f_co2} -> {f_species}: value / {value} / CO2 * {symbol} \n')
+    # open file and write header
+    gwp = open(f'gwp_{context}.txt', 'w')
 
-gwp.write('@end')
-gwp.close()
+    gwp.writelines(f'# This file was created using the script `{__file__}`\n')
+    gwp.write('# DO NOT ALTER THIS FILE MANUALLY!\n\n')
+    gwp.write(f'@context gwp_{context}\n')
+
+    for i, (species, symbol, value) in _data.iterrows():
+        gwp.write('\n')
+        gwp.write(f'# {species} - {symbol}\n\n')
+        for f in formatters:
+            f_gas, f_co2 = f.format(species), f.format('carbon_dioxide')
+            gwp.write(
+                f'    {f_gas} -> {f_co2}: value * {value} * CO2 / {symbol}\n')
+            gwp.write(
+                f'    {f_co2} -> {f_gas}: value / {value} * {symbol} / CO2\n')
+
+    gwp.write('\n')
+    gwp.write('@end')
+    gwp.close()
