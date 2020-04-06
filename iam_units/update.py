77e30b5abc43949432125e9e848aacaea54f8994
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-06 20:37:06 +0200

    Also generate an importable regex for emissions species

--- iam_units/update.py
+++ iam_units/update.py
@@ -4,13 +4,16 @@ import pandas as pd
 import sys
 
 
-# Base path for all package data
-DATA_PATH = Path(__file__).parent / 'data'
+# Base path for package code
+BASE_PATH = Path(__file__).parent
+
+# Base path for package data
+DATA_PATH = BASE_PATH / 'data'
 
 
 # Format strings for emissions()
 _EMI_HEADER = """# This file was generated using:
-    python -m iam_units.update emissions
+#    python -m iam_units.update emissions
 # DO NOT ALTER THIS FILE MANUALLY!
 """
 
@@ -21,9 +24,18 @@ _EMI_DATA = f"""{_EMI_HEADER}
     [mass] / [time] -> [_GWP] / [time]: value * (_a * _gwp / kg)
     [_GWP] / [time] -> [mass] / [time]: value / (_a * _gwp / kg)
 
-
     {{defs}}
 @end
+"""
+
+_EMI_CODE = f"""{_EMI_HEADER}
+import re
+
+SPECIES = [
+    '{{symbols}}',
+    ]
+
+pattern = re.compile('(?<=[ -])(' + '|'.join(SPECIES) + ')(?=[ -/]?)')
 """
 
 
@@ -41,9 +53,14 @@ def emissions():
              .dropna(subset=['value'])
 
     # Write the file containing the species defs
+    symbols = sorted(data['Symbol'].unique())
     with open(data_path / 'species.txt', 'w') as f:
         f.write(_EMI_HEADER + '\n')
-        [f.write(f'a_{s} = NaN\n') for s in sorted(data['Symbol'].unique())]
+        [f.write(f'a_{symbol} = NaN\n') for symbol in symbols]
+
+    # Write a regular expression containing the species names
+    symbols = "',\n    '".join(symbols)
+    (BASE_PATH / 'emissions.py').write_text(_EMI_CODE.format(**locals()))
 
     # Write one file containing a context for each metric
     for metric, _data in data.groupby('metric'):
