748079bcd5f85e793bcccbc0e78282a71c8bffb0
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-21 11:59:09 +0200

    Copyedit comments and docstring of convert_gwp()

--- iam_units/__init__.py
+++ iam_units/__init__.py
@@ -29,39 +29,38 @@ def convert_gwp(metric, quantity, *species):
         Metric conversion factors to use. May be :obj:`None` if the input and
         output species are the same.
     quantity : str or pint.Quantity or tuple
-        Quantity to convert. If a tuple, the arguments are passed to the
-        :class:`pint.Quantity` constructor.
+        Quantity to convert. If a tuple of (magnitude, unit), these are passed
+        as arguments to :class:`pint.Quantity`.
     species : sequence of str, length 1 or 2
-        Output, or (input, output) species, e.g. ('CH4', 'CO2') to convert
-        mass of CH₄ to GWP-equivalent mass of CO₂. If only the output species
-        is provided, *quantity* must contain the name of the input species in
-        some location, e.g. 'tonne CH4 / year'.
+        Output, or (input, output) species symbols, e.g. ('CH4', 'CO2') to
+        convert mass of CH₄ to GWP-equivalent mass of CO₂. If only the output
+        species is provided, *quantity* must contain the symbol of the input
+        species in some location, e.g. '1.0 tonne CH4 / year'.
 
     Returns
     -------
     pint.Quantity
         `quantity` converted from the input to output species.
     """
-    # Handle *species*
+    # Handle *species*: either (in, out) or only out
     try:
-        # Split a 2-tuple
         species_in, species_out = species
     except ValueError:
         if len(species) != 1:
             raise ValueError('Must provide (from, to) or (to,) species')
-        # Only output emissions species provided
         species_in, species_out = None, species[0]
 
-    # Split *quantity* if it is a tuple:
+    # Split *quantity* if it is a tuple. After this step:
     # - *mag* is the magnitude, or None.
     # - *expr* is a string expression for either just the units, or the entire
-    #   quantity including magnitude.
+    #   quantity, including magnitude, as a str or pint.Quantity.
     mag, expr = quantity if isinstance(quantity, tuple) else (None, quantity)
 
+    # If species_in wasn't provided, then *expr* must contain it
     if not species_in:
-        # *expr* must contain the input species; extract it using the regex
+        # Extract it using the regex, then re-assemble the expression for the
+        # units or whole quantity
         q0, species_in, q1 = emissions.pattern.split(expr, maxsplit=1)
-        # Re-assemble the expression for the units or whole quantity
         expr = q0 + q1
 
     # *metric* can only be None if the input and output species symbols are
@@ -71,26 +70,26 @@ def convert_gwp(metric, quantity, *species):
                 any({species_in, species_out} <= g for g in emissions.EQUIV)):
             metric = 'AR5GWP100'
         elif species_in in species_out:
-            # Both a DimensionalityError ('CO2' → 'CO2 / a') and a ValueError
-            # (no metric); raise the former for pyam compat
+            # Eg. 'CO2' in 'CO2 / a'. This is both a DimensionalityError and a
+            # ValueError (no metric); raise the former for pyam compat
             raise pint.DimensionalityError(species_in, species_out)
         else:
             msg = f'Must provide GWP metric for ({species_in}, {species_out})'
             raise ValueError(msg)
 
     # Ensure a pint.Quantity object:
-    # - If tuple input was given, use the 2-arg constructor.
-    # - If not, use the 1-arg form to convert a string.
-    # - If the input was already a Quantity, this is a no-op.
+    # - If *quantity* was a tuple, use the 2-arg constructor.
+    # - If a str, use the 1-arg form to parse it.
+    # - If already a pint.Quantity, this is a no-op.
     args = (expr,) if mag is None else (mag, expr)
     quantity = registry.Quantity(*args)
 
-    # Intermediate units with the same dimensionality as *quantity*, except
-    # '[mass]' replaced with the dummy unit '_gwp'
+    # Construct intermediate units with the same dimensionality as *quantity*,
+    # except '[mass]' replaced with the dummy unit '_gwp'
     dummy = quantity.units / registry.Unit('tonne / _gwp')
 
-    # Convert to GWP dummy units using 'a' for the input species; then back to
-    # the input units using 'a' for the output species.
+    # Convert to dummy units using 'a' for the input species; then back to the
+    # input units using 'a' for the output species.
     return quantity.to(dummy, metric, _a=f'a_{species_in}') \
                    .to(quantity.units, metric, _a=f'a_{species_out}')
 
