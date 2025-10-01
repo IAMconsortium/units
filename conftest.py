099434c44b65c4da95b08f958214eb0ef5bc710e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-10-01 15:58:26 +0200

    Set IAM_UNITS_CACHE for the test session

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import os
+
+
+def pytest_sessionstart(session) -> None:
+    """Set environment variables for test session."""
+
+    # Use a pytest temporary directory for cache, instead of the user's cache
+    p = session.config._tmp_path_factory.mktemp("pint-cache", numbered=False)
+    os.environ.setdefault("IAM_UNITS_CACHE", str(p))
