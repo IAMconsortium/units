1716c90dbecb98bb14159a0171814fb62036d4cc
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-10-01 15:32:47 +0200

    Type hint __init__.py
    - Rewrap docstrings and comments.
    - Replace *-emphasis with ` in docstrings.

--- iam_units/__init__.py
+++ iam_units/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from pathlib import Path
+from typing import TYPE_CHECKING, Any, Optional, Union
 from warnings import warn
 
 import pint
@@ -10,6 +11,14 @@ from pint.util import to_units_container
 from . import emissions
 from .currency import configure_currency
 
+if TYPE_CHECKING:
+    from decimal import Decimal
+    from fractions import Fraction
+
+    import numpy as np
+
+    Scalar = Union[float, int, Decimal, Fraction, np.number[Any]]
+
 __all__ = [
     "convert_gwp",
     "configure_currency",
@@ -18,23 +27,27 @@ __all__ = [
 ]
 
 
-def convert_gwp(metric, quantity, *species):
-    """Convert *quantity* between GHG *species* with a GWP *metric*.
+def convert_gwp(
+    metric: Optional[str],
+    quantity: Union[str, "pint.Quantity", tuple[float, str]],
+    *species: str,
+) -> "pint.Quantity":
+    """Convert `quantity` between GHG `species` with a GWP `metric`.
 
     Parameters
     ----------
     metric : str or None
-        Metric conversion factors to use. May be :obj:`None` if the input and
-        output species are the same.
-        Use :code:`iam_units.emissions.METRICS` for a list of available metrics.
+        Metric conversion factors to use. May be :obj:`None` if the input and output
+        species are the same. Use :code:`iam_units.emissions.METRICS` for a list of
+        available metrics.
     quantity : str or pint.Quantity or tuple
-        Quantity to convert. If a tuple of (magnitude, unit), these are passed
-        as arguments to :class:`pint.Quantity`.
+        Quantity to convert. If a tuple of (magnitude, unit), these are passed as
+        arguments to :class:`pint.Quantity`.
     species : sequence of str, length 1 or 2
-        Output, or (input, output) species symbols, e.g. ('CH4', 'CO2') to
-        convert mass of CH₄ to GWP-equivalent mass of CO₂. If only the output
-        species is provided, *quantity* must contain the symbol of the input
-        species in some location, e.g. '1.0 tonne CH4 / year'.
+        Output, or (input, output) species symbols, e.g. ('CH4', 'CO2') to convert mass
+        of CH₄ to GWP-equivalent mass of CO₂. If only the output species is provided,
+        `quantity` must contain the symbol of the input species in some location, e.g.
+        '1.0 tonne CH4 / year'.
 
     Returns
     -------
@@ -44,11 +57,11 @@ def convert_gwp(metric, quantity, *species):
     Notes
     -----
     The conversion factors are taken from the `globalwarmingpotentials` package,
-    see https://github.com/openclimatedata/globalwarmingpotentials.
-    You can use :code:`iam_units.emissions.GWP_VERSION` to check from which version
-    of that package the conversion tables in the `iam_units` package were generated.
+    see https://github.com/openclimatedata/globalwarmingpotentials. You can use
+    :code:`iam_units.emissions.GWP_VERSION` to check from which version of that package
+    the conversion tables in the `iam_units` package were generated.
     """
-    # Handle *species*: either (in, out) or only out
+    # Handle `species`: either (in, out) or only out
     try:
         species_in, species_out = species
     except ValueError:
@@ -56,20 +69,20 @@ def convert_gwp(metric, quantity, *species):
             raise ValueError("Must provide (from, to) or (to,) species")
         species_in, species_out = None, species[0]
 
-    # Split *quantity* if it is a tuple. After this step:
-    # - *mag* is the magnitude, or None.
-    # - *expr* is a string expression for either just the units, or the entire
+    # Split `quantity` if it is a tuple. After this step:
+    # - `mag` is the magnitude, or None.
+    # - `expr` is a string expression for either just the units, or the entire
     #   quantity, including magnitude, as a str or pint.Quantity.
     mag, expr = quantity if isinstance(quantity, tuple) else (None, quantity)
 
-    # If species_in wasn't provided, then *expr* must contain it
+    # If species_in wasn't provided, then `expr` must contain it
     if not species_in:
         # Extract it using the regex, then re-assemble the expression for the
         # units or whole quantity
-        q0, species_in, q1 = emissions.pattern.split(expr, maxsplit=1)
+        q0, species_in, q1 = emissions.pattern.split(str(expr), maxsplit=1)
         expr = q0 + q1
 
-    # *metric* can only be None if the input and output species symbols are
+    # `metric` can only be None if the input and output species symbols are
     # identical or equivalent
     if metric is None:
         if species_in == species_out or any(
@@ -85,26 +98,28 @@ def convert_gwp(metric, quantity, *species):
             raise ValueError(msg)
 
     # Ensure a pint.Quantity object:
-    # - If *quantity* was a tuple, use the 2-arg constructor.
+    # - If `quantity` was a tuple, use the 2-arg constructor.
     # - If a str, use the 1-arg form to parse it.
     # - If already a pint.Quantity, this is a no-op.
     args = (expr,) if mag is None else (mag, expr)
     quantity = registry.Quantity(*args)
 
-    # Construct intermediate units with the same dimensionality as *quantity*,
-    # except '[mass]' replaced with the dummy unit '_gwp'
+    # Construct intermediate units with the same dimensionality as `quantity`, except
+    # '[mass]' replaced with the dummy unit '_gwp'
     m_dim = quantity.dimensionality["[mass]"]
     dummy = quantity.units / registry.Unit(f"tonne ** {m_dim} / _gwp")
 
-    # Convert to dummy units using 'a' for the input species; then back to the
-    # input units using 'a' for the output species.
+    # Convert to dummy units using 'a' for the input species; then back to the input
+    # units using 'a' for the output species.
     return quantity.to(dummy, metric, _a=f"a_{species_in}").to(
         quantity.units, metric, _a=f"a_{species_out}"
     )
 
 
-def format_mass(obj, info, spec=None):
-    """Format the units of *obj* with *info* inserted after its mass unit.
+def format_mass(
+    obj: Union["pint.Quantity", "pint.Unit"], info: str, spec: Optional[str] = None
+) -> str:
+    """Format the units of `obj` with `info` inserted after its mass unit.
 
     Parameters
     ----------
@@ -117,22 +132,22 @@ def format_mass(obj, info, spec=None):
     """
     spec = spec or registry.formatter.default_format
 
-    try:
+    if hasattr(obj, "units"):
         # Use only the units of a Quantity object
-        obj = obj.units
-    except AttributeError:
-        pass  # Already a Unit object
+        _obj = obj.units
+    else:
+        _obj = obj
 
     # Use the symbol if the modifier "~" is in `spec`; else the canonical name. cf.
     # pint.Unit.__format__()
     method = registry._get_symbol if "~" in spec else lambda k: k
     # Collect the pieces of the unit expression
-    units = [[method(key), value] for key, value in obj._units.items()]
+    units: list[tuple[str, "Scalar"]] = [(method(k), v) for k, v in obj._units.items()]
 
     # Index of the mass component
     mass_index = list(obj.dimensionality.keys()).index("[mass]")
     # Append the information (e.g. species) to the mass component
-    units[mass_index][0] += f" {info}"
+    units[mass_index] = (f"{units[mass_index][0]} {info}", units[mass_index][1])
 
     # Prepare pint.util.UnitsContainer and hand off to pint's formatting. Discard "~"
     # (used above) and ":" (invalid in pint ≥0.18).
